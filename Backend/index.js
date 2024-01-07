30d14a5f9ca2dc66dae800a7207d8c91392ecd84
Pankaj Kumar <pkthapliyal101@gmail.com>
2024-01-08 00:55:14 +0530

    Final Cors origin

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const { initializeTutorSocket } = require('./helpers/handleDoubt');
 // new socket server
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000", "https://doubt-share-rouge.vercel.app/"],
+        origin: "https://doubt-share-919bm8jez-pkthapliyal.vercel.app/",
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
     }
 })
